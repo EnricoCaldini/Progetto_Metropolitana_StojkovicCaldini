59020e4bc3e4c4485fb4bbf4ff05e31032518f07
Danilo <danilostojkovic@hotmail.com>
2019-02-07 05:17:51 +0100

    loloolol
    cancro alla carotide

--- metropolitana/metropolitana/Form1.cs
+++ metropolitana/metropolitana/Form1.cs
@@ -12,21 +12,88 @@ namespace metropolitana
 {
     public partial class Form1 : Form
     {
+        bool createStation = false;
+        bool createLink = false;
+        List<Stazione> staz = new List<Stazione>();
+        Stazione uno = null;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Click(object sender, EventArgs e)
+        private void panel1_Click(object sender, EventArgs e)
         {
-            PictureBox picture = new PictureBox
+            int x = Cursor.Position.X - 38;
+            int y = Cursor.Position.Y - 140;
+            if (createStation)
             {
-                Name = "pictureBox",
-                Size = new Size(50, 50),
-                Location = new Point(100, 100),
-                Image = Image.FromFile("42534-metro-icon.png"),
-            };
-            this.Controls.Add(picture);
+                PictureBox picture = new PictureBox
+                {
+                    Name = "pictureBox",
+                    Size = new Size(50, 50),
+                    Location = new Point(x, y),
+                    BackgroundImage = Image.FromFile("42534-metro-icon.png"),
+                    BackgroundImageLayout = ImageLayout.Zoom,
+                };
+                picture.MouseClick += stazioneClick;
+                panel1.Controls.Add(picture);
+                picture.Tag = staz.Count;
+                staz.Add(new Stazione(x + 25, y + 25));
+            }
+        }
+
+        private void stazioneClick(object sender, MouseEventArgs e)
+        {
+            PictureBox pic = (PictureBox)sender;
+            Stazione sel = staz[Convert.ToInt32(pic.Tag)];
+            if (createLink)
+            {
+                if (uno == null)
+                    uno = sel;
+                else
+                {
+                    Graphics gs = panel1.CreateGraphics();
+                    Pen pn = new Pen(Color.Cyan, 3);
+                    gs.DrawLine(pn, uno.x, uno.y, sel.x, sel.y);
+                    uno = null;
+                }
+            }
+            else if(!createStation)
+            {
+                VisualStazione vs = new VisualStazione(sel);
+                vs.ShowDialog();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (createStation)
+                button1.BackColor = Color.White;
+            else
+            {
+                button1.BackColor = Color.DarkGray;
+                button2.BackColor = Color.White;
+                uno = null;
+                createLink = false;
+            }
+            createStation = !createStation;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (createLink)
+            {
+                button2.BackColor = Color.White;
+                uno = null;
+            }
+            else
+            {
+                button2.BackColor = Color.DarkGray;
+                button1.BackColor = Color.White;
+                createStation = false;
+            }
+            createLink = !createLink;
         }
     }
 }
